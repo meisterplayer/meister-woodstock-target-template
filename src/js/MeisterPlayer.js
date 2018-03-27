e9e8b0ab746fa4fb30a0b86d01a858e247a742c0
Ivo Toby <i.toby@wearetriple.com>
2018-03-27 10:25:04 +0200

    update plugins setup for RP

--- src/js/MeisterPlayer.js
+++ src/js/MeisterPlayer.js
@@ -1,28 +1,32 @@
+import 'babel-polyfill';
 import Meister from '@meisterplayer/meisterplayer/index';
 import BaseMedia from '@meisterplayer/plugin-basemedia/index';
 import Html5Player from '@meisterplayer/plugin-html5player/index';
-import StandardUi from '@meisterplayer/plugin-standardui/index';
 import DebugOverlay from '@meisterplayer/plugin-debugoverlay/index';
 import NativeHls from '@meisterplayer/plugin-nativehls/index';
 import Hls from '@meisterplayer/plugin-hls/index';
 import Dash from '@meisterplayer/plugin-dash/index';
 import MultiSource from '@meisterplayer/plugin-multisource/index';
 import WebVtt from '@meisterplayer/plugin-webvtt/index';
-import Smooth from '@meisterplayer/plugin-smooth/index';
-
-
+import Watermark from  '@meisterplayer/plugin-watermark';
+import HtmlUI from '@meisterplayer/plugin-htmlui';
 
 Meister.builtIn = {
     [Html5Player.pluginName]: {},
     [BaseMedia.pluginName]: {},
-    [StandardUi.pluginName]: {},
     [NativeHls.pluginName]: {},
     [Hls.pluginName]: {},
     [Dash.pluginName]: {},
     [MultiSource.pluginName]: {},
     [WebVtt.pluginName]: {},
-    [Smooth.pluginName]: {},
     [DebugOverlay.pluginName]: {},
+    [HtmlUI.pluginName]: {},
+    [Watermark.pluginName]: {
+        iconUrl: 'https://www.meisterplayer.com/static/media/logotype-meister-invert-mini.png',
+        style: {
+            maxWidth: '65px',
+        },
+    },
 };
 
 Meister.Configuration.overwrite(Meister.builtIn);
